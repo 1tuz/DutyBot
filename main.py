66268b3a62a9ef75b5161108a667a0156a28e95a
Antony <58532577+1tuz@users.noreply.github.com>
2023-05-25 16:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import os
-from dotenv import load_dotenv
 import telegram
+from dotenv import load_dotenv
 import asyncio
 
 load_dotenv()
-
 bot_token = os.getenv('TOKEN')
 group_id = int(os.getenv('GROUP_ID'))
-usernames = [username.replace('"', '').replace("'", '').replace('[', '').replace(']', '').replace(',', '').strip() for username in os.getenv('USERS').split(',')]
-starting_user_index = int(os.getenv('START_INDEX'))
+usernames = [username.replace('"', '').replace("'", '').replace('[', '').replace(']', '').replace(',', '').strip() for
+             username in os.getenv('USERS').split(',')]
+starting_user_index = 0
 
 bot = telegram.Bot(token=bot_token)
 
-async def main():
+
+def save_indexes(start_index):  # Сохраняем индексы
+    with open('indexes.txt', 'w') as f:
+        f.write(f"START_INDEX={start_index}")
+
+
+def load_indexes():  # Загружаем индексы
+    with open('indexes.txt', 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        key, value = line.strip().split('=')
+        if key == 'START_INDEX':
+            return int(value)
+
+
+async def main():  # Используем асинхронность в функции main()
     global starting_user_index
+
+    starting_user_index = load_indexes()
 
     next_duty_index = (starting_user_index + 1) % len(usernames)
     next_duty = usernames[next_duty_index]
 
     message_text = f'Сегодня дежурный: {next_duty}'
-    await bot.send_message(chat_id=group_id, text=message_text)
+    await bot.send_message(chat_id=group_id, text=message_text)  # Добавляем await
 
     starting_user_index = next_duty_index
-    os.environ['START_INDEX'] = str(starting_user_index)
-    with open('.env', 'w') as f:
-        f.write(f"TOKEN={bot_token}\n")
-        f.write(f"USERS={','.join(usernames)}\n")
-        f.write(f"GROUP_ID={group_id}\n")
-        f.write(f"START_INDEX={starting_user_index}\n")
+    save_indexes(starting_user_index)
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+
+    asyncio.run(main())  # Запускаем асинхронную функцию с помощью asyncio.run()
