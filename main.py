dcb72967d90ea44fbfcfc06e84e58dae5ad9ee8c
Antony <58532577+1tuz@users.noreply.github.com>
2023-07-26 21:32:28 +0300

    Update main.py
    Убрал асинхронность, пакет для бота, добавил requests, а также добавил тайм-ауты.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
-import telegram
+import requests
 from dotenv import load_dotenv
-import asyncio
+import logging
+from datetime import datetime, timedelta
+import time
 
 load_dotenv()
 bot_token = os.getenv('TOKEN')
@@ -10,39 +12,89 @@ usernames = [username.replace('"', '').replace("'", '').replace('[', '').replace
              username in os.getenv('USERS').split(',')]
 starting_user_index = 0
 
-bot = telegram.Bot(token=bot_token)
+# Set up logging
+logging.basicConfig(filename='duty_bot.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def save_indexes(start_index):  # Сохраняем индексы
-    with open('indexes.txt', 'w') as f:
-        f.write(f"START_INDEX={start_index}")
+def save_indexes(start_index, counter):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, 'indexes.txt')
+    with open(file_path, 'w') as f:
+        f.write(f"START_INDEX={start_index}\n")
+        f.write(f"COUNTER={counter}")
 
 
-def load_indexes():  # Загружаем индексы
-    with open('indexes.txt', 'r') as f:
+def load_indexes():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, 'indexes.txt')
+
+    if not os.path.isfile(file_path):
+        return None, None
+
+    with open(file_path, 'r') as f:
         lines = f.readlines()
 
+    start_index = None
+    counter = None
     for line in lines:
         key, value = line.strip().split('=')
         if key == 'START_INDEX':
-            return int(value)
+            start_index = int(value)
+        elif key == 'COUNTER':
+            counter = int(value)
+
+    return start_index, counter
 
 
-async def main():  # Используем асинхронность в функции main()
-    global starting_user_index
+def send_duty_message():
+    starting_user_index, counter = load_indexes()
 
-    starting_user_index = load_indexes()
+    if starting_user_index is None or counter is None:
+        starting_user_index = 0
+        counter = 0
 
-    next_duty_index = (starting_user_index + 1) % len(usernames)
+    next_duty_index = (starting_user_index + counter) % len(usernames)
     next_duty = usernames[next_duty_index]
 
-    message_text = f'Всем доброго времени суток!\nСегодня дежурный: {next_duty}'
-    await bot.send_message(chat_id=group_id, text=message_text)  # Добавляем await
+    message_text = f'Всем доброго времени суток!\nНапоминаю, что сегодня дежурный {next_duty}'
 
-    starting_user_index = next_duty_index
-    save_indexes(starting_user_index)
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    data = {
+        'chat_id': group_id,
+        'text': message_text
+    }
+
+    response = requests.post(url, data=data)
+    if response.ok:
+        logging.info('Duty message sent.')
+    else:
+        logging.error('Failed to send duty message.')
+
+    counter += 1
+    if counter == 2:
+        counter = 0
+        starting_user_index = (starting_user_index + 1) % len(usernames)
+
+    save_indexes(starting_user_index, counter)
+
+
+def run_duty_bot():
+    while True:
+        # Get the current time
+        now = datetime.now()
+
+        # Тест тайм-аутов
+        if now.hour in [7, 8, 9, 10, 16, 22, 23]:
+            send_duty_message()
+
+        # Sleep for 1 hour before checking again
+        time.sleep(3600)
 
 
 if __name__ == '__main__':
-
-    asyncio.run(main())  # Запускаем асинхронную функцию с помощью asyncio.run()
+    # Run the duty bot indefinitely
+    try:
+        run_duty_bot()
+    except KeyboardInterrupt:
+        pass
